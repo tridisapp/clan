aada6c48884c9c69144f2eacf8de12e188821460
SystemSAD <massotpierrenicolas@gmail.com>
2025-07-14 18:41:11 +0200

    feat: allow user profile customization

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true }
+  passwordHash: { type: String, required: true },
+  avatar: String,
+  banner: String
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
